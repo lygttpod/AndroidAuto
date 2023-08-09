d1ff2a89a045f49e0af53a2eb67947a7133c7c64
Allen <lygttpod@163.com>
2023-08-09 21:41:29 +0800

    放开建群检测逻辑代码

--- app/src/main/java/com/android/wechat/tools/helper/TaskByGroupHelper.kt
+++ app/src/main/java/com/android/wechat/tools/helper/TaskByGroupHelper.kt
@@ -44,7 +44,10 @@ object TaskByGroupHelper {
         Log.d("LogTracker", "startCheckByCreateGroup: 任务执行结束，耗时${taskEnd.formatTime()}")
         Log.d("result", "${alreadyCheckedUsers}")
         //任务结束返回APP
-        wxAccessibilityService?.pressBackButton()
+        val isHome = WXHomePage.backToHome()
+        if (isHome) {
+            wxAccessibilityService?.pressBackButton()
+        }
     }
 
     private suspend fun singleTask() {
@@ -68,54 +71,49 @@ object TaskByGroupHelper {
         Log.d("LogTracker", "selectUser: ${selectUser?.size}")
         //当发现选择建群的用户少于2个人的时候，无法建群(即选择一个人是无法拉群的)
         if (selectUser == null || selectUser.size < 2) {
-            wxAccessibilityService?.pressBackButton()
+            WXHomePage.backToHome()
             return
         }
+        //点击完成，开始建群
+        val isClickComplete = WXCreateGroupPage.clickCompleteBtn()
+        if (!isClickComplete) return
+        //判断是否进入到群聊页，因为创建群需要一定的时间
+        val inGroupCHatPage = WXGroupChatPage.inPage()
+        if (!inGroupCHatPage) return
+        //在群里判断好友状态
+        val result = WXGroupChatPage.checkUserStatus()
+        FriendStatusHelper.addCheckResults(result)
+
+        //=======检测完毕开始解散群和删除群=======
+
+        //点击群聊右上角按钮
+        val isClickMore = WXGroupChatPage.clickMoreBtn()
+        if (!isClickMore) return
+        //点击群管理按钮
+        val clickGroupManager = WXGroupInfoPage.clickGroupManager()
+        if (!clickGroupManager) return
+        //点击解散该群聊按钮
+        val clickDisbandGroup = WXGroupManagerPage.clickDisbandGroup()
+        if (!clickDisbandGroup) return
+        //点击弹框确定解散按钮
+        val clickDisbandGroupDialog = WXGroupManagerPage.clickDisbandGroupDialog()
+        if (!clickDisbandGroupDialog) return
+        //再次点击群聊右上角按钮
+        val clickMoreAgain = WXGroupChatPage.clickMoreBtn()
+        if (!clickMoreAgain) return
+        //点击删除按钮
+        val clickDeleteGroup = WXGroupInfoPage.clickDeleteGroup()
+        if (!clickDeleteGroup) return
+        //点击弹框确定删除按钮
+        val clickDeleteGroupDialog = WXGroupInfoPage.clickDeleteGroupDialog()
+        if (!clickDeleteGroupDialog) return
+
+        delay(2000)
+
         //未监测完就循环执行以上步骤
         if (!isCheckedFinished.get()) {
             singleTask()
         }
-
-//        //点击完成，开始建群
-//        val isClickComplete = WXCreateGroupPage.clickCompleteBtn()
-//        if (!isClickComplete) return
-//        //判断是否进入到群聊页，因为创建群需要一定的时间
-//        val inGroupCHatPage = WXGroupChatPage.inPage()
-//        if (!inGroupCHatPage) return
-//        //在群里判断好友状态
-//        val result = WXGroupChatPage.checkUserStatus()
-//        FriendStatusHelper.addCheckResults(result)
-//
-//        //=======检测完毕开始解散群和删除群=======
-//
-//        //点击群聊右上角按钮
-//        val isClickMore = WXGroupChatPage.clickMoreBtn()
-//        if (!isClickMore) return
-//        //点击群管理按钮
-//        val clickGroupManager = WXGroupInfoPage.clickGroupManager()
-//        if (!clickGroupManager) return
-//        //点击解散该群聊按钮
-//        val clickDisbandGroup = WXGroupManagerPage.clickDisbandGroup()
-//        if (!clickDisbandGroup) return
-//        //点击弹框确定解散按钮
-//        val clickDisbandGroupDialog = WXGroupManagerPage.clickDisbandGroupDialog()
-//        if (!clickDisbandGroupDialog) return
-//        //再次点击群聊右上角按钮
-//        val clickMoreAgain = WXGroupChatPage.clickMoreBtn()
-//        if (!clickMoreAgain) return
-//        //点击删除按钮
-//        val clickDeleteGroup = WXGroupInfoPage.clickDeleteGroup()
-//        if (!clickDeleteGroup) return
-//        //点击弹框确定删除按钮
-//        val clickDeleteGroupDialog = WXGroupInfoPage.clickDeleteGroupDialog()
-//        if (!clickDeleteGroupDialog) return
-//
-//        delay(2000)
-//
-//        //未监测完就循环执行以上步骤
-//        if (!isCheckedFinished.get()) {
-//            singleTask()
-//        }
 
         //本次检测流程全部执行完毕
     }
